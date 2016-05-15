d0d5b01fcfa0001167cf7cc6a4a2e62a6006fb2a
Josenildo Silva <hygenscolt@yahoo.com>
2016-05-15 00:17:14 -0300

    Extremelly simple table concat
    Execute concatenation for two tables without metatables.

---
+++ concat_two_tables.lua
@@ -0,0 +1,8 @@
+local function concatTables(t,s)
+    for i = 1,#s do table.insert(t,rawget(s,i)) end
+    return t
+end
+t = {1000,55,331}
+s = {445,568,679,777,888}
+t = concatTables(t,s) 
+for x = 1,#t do print (t[x]) end
