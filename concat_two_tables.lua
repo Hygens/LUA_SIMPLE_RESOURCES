b4bee5773b2e18e37281f7911d814268003f0c5b
Josenildo Silva <hygenscolt@yahoo.com>
2016-05-15 00:22:57 -0300

    Add files via upload

--- concat_two_tables.lua
+++ concat_two_tables.lua
@@ -4,5 +4,12 @@ local function concatTables(t,s)
 end
 t = {1000,55,331}
 s = {445,568,679,777,888}
+-- table s tail in t
 t = concatTables(t,s) 
+for x = 1,#t do print (t[x]) end
+print()
+t = {1000,55,331}
+s = {445,568,679,777,888}
+--table s prepend in t
+t = concatTables(s,t)
 for x = 1,#t do print (t[x]) end
